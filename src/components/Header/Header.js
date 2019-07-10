decd5389de8321fefa26c671d3de691e6821acd3
Michael Weber <mscottweber@gmail.com>
2019-07-10 14:48:32 -0500

    Adjusting design and animations

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import {Spring, config} from 'react-spring/renderprops';
 import Title from './Title';
 import MenuButton from './MenuButton';
 
@@ -7,15 +7,26 @@ import './Header.css';
 
 const Header = ({ onSectionClick, onMenuBtnClick }) => {
     return (
-        <div className="header">
-            <Title 
-                siteTitle='Michael Weber Music'
-                onSectionClick={onSectionClick}
-            />
-            <MenuButton 
-                onMenuBtnClick={onMenuBtnClick}
-            />
-        </div>
+        <Spring
+            from={{ opacity: 0, transform: 'translate3d(0,-50%,0)' }}
+            to={{ opacity: 1, transform: 'translate3d(0,0,0)' }}
+            config={ config.stiff }
+        >
+            {props => (
+                <div 
+                    style={props}
+                    className="header"
+                >
+                    <Title 
+                        siteTitle='Michael Weber Music'
+                        onSectionClick={onSectionClick}
+                    />
+                    <MenuButton 
+                        onMenuBtnClick={onMenuBtnClick}
+                    />
+                </div>           
+            )}
+        </Spring>
     )
 }
 
